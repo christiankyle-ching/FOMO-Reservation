c49bb8749059ab577bc4d02e650f8b71a7800dff
Christian Kyle <57316283+christiankyle-ching@users.noreply.github.com>
2021-04-02 20:05:51 +0800

    Removed Console Logs in Production

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,16 @@
-module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
+const _plugins = [];
+
+// Remove console.log in production
+if (process.env.NODE_ENV === "production") {
+  _plugins.push([
+    "transform-remove-console",
+    {
+      exclude: ["error", "warn"],
+    },
+  ]);
 }
+
+module.exports = {
+  presets: ["@vue/cli-plugin-babel/preset"],
+  plugins: _plugins,
+};
